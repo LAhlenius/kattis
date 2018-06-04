8632701f990116194a223ac807097dc9ea716a76
linus <linkanahlenius@hotmail.com>
2018-06-04 18:41:34 +0200

    initial commit, not done

---
+++ c/racing_around_the_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+double distance_between_chars(char a, char b)
+{
+	char distance = abs(a-b);
+	if(distance > 13){
+		//Not possible due to " " and "'".
+	}
+}
+
+
+int main()
+{
+	
+}
